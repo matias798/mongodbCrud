c43c66908000aa328fe46cf38221b9ed6e55319f
s <s>
2021-06-10 15:27:12 -0300

    now we can check if a task is completed or not

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,30 +8,40 @@ router.get("/", async function (req, res) {
   res.render("index", { tasks });
 });
 
-/* Adding new task home page. */
+/* Adding new task to home page */
 router.post("/add", async function (req, res) {
   const task = new Task(req.body);
   await task.save();
   res.redirect("/");
 });
 
-
-router.get('/edit/:id', async (req, res, next) => {
+/* Edit view next( GET ) */
+router.get("/edit/:id", async (req, res, next) => {
   const task = await Task.findById(req.params.id);
-  console.log(task)
-  res.render('edit', { task });
+  console.log(task);
+  res.render("edit", { task });
 });
 
-router.post('/edit/:id', async (req, res, next) => {
+/* Edit view next( POST ) */
+router.post("/edit/:id", async (req, res, next) => {
   const { id } = req.params;
-  await Task.update({_id: id}, req.body);
-  res.redirect('/');
+  await Task.update({ _id: id }, req.body);
+  res.redirect("/");
 });
 
 /* Delete selected task */
 router.get("/delete/:id", async (req, res, next) => {
   let { id } = req.params;
   await Task.remove({ _id: id });
+  res.redirect("/");
+});
+
+/* change the task state */
+router.get("/turn/:id", async (req, res, next) => {
+  let { id } = req.params;
+  const task = await Task.findById(id);
+  task.status = !task.status;
+  await task.save();
   res.redirect("/");
 });
 
