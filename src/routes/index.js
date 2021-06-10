3e9749ac61a7d396feca7770b20c68a08570a441
s <s>
2021-06-10 14:47:37 -0300

    now we can delete a task

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Task = require("../models/task");
 
-const Task = require('../models/task')
 /* GET home page. */
-router.get('/',async function(req, res, next) {
-  const tasks= await Task.find();
-  res.render('index',{tasks});
+router.get("/", async function (req, res) {
+  const tasks = await Task.find();
+  res.render("index", { tasks });
 });
-/* Post home page. */
-router.post('/add', async function(req, res, next) {
-const task = new Task(req.body);
-await task.save()
-res.redirect('/')
+
+/* Adding new task home page. */
+router.post("/add", async function (req, res) {
+  const task = new Task(req.body);
+  await task.save();
+  res.redirect("/");
+});
+
+/* Delete selected task */
+router.get("/delete/:id", async (req, res, next) => {
+  let { id } = req.params;
+  await Task.remove({ _id: id });
+  res.redirect("/");
 });
 
 module.exports = router;
