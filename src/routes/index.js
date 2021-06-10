d8c1d566e2783bd3b610efad8cd9813db88b8431
s <s>
2021-06-10 12:28:50 -0300

    created main page, models and creation part of the crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const Task = require('../models/task')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+/* Post home page. */
+router.post('/add', async function(req, res, next) {
+const task = new Task(req.body);
+await task.save()
+res.send(req.body)
 });
 
 module.exports = router;
